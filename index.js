2a2d6bbc83e87b5e0120520704e2d3d4710597c3
Marcio <marcioxerfan@gmail.com>
2023-04-18 20:55:55 -0300

    Feat: conexao com DB.

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const connectToDatabase = require("./src/database/database");
+
+const app = express();
+
+const port = 3000;
+
+app.use(express.json());
+
+connectToDatabase();
+
+app.get("/", (req, res) => {
+    res.send({
+        message: "Bem vindo ao nosso Market-Place"
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+}); 
