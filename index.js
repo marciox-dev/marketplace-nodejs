ad717dcd943453d404b276df129a7ded84832da4
Marcio <marcioxerfan@gmail.com>
2023-04-18 21:34:12 -0300

    feat: controler

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const connectToDatabase = require("./src/database/database");
 
+const usuario = require("./src/router/usuario.router");
+
 const app = express();
 
 const port = 3000;
 
 app.use(express.json());
 
+
+
 connectToDatabase();
+
+
+app.use("/usuario", usuario);
 
 app.get("/", (req, res) => {
     res.send({
