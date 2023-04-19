87488048066442ef87f2009559e1cc2e3971ab34
Marcio <marcioxerfan@gmail.com>
2023-04-19 09:10:31 -0300

    Feat: CRUD user service

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const connectToDatabase = require("./src/database/database");
+const connectToDatabase = require("./src/database/database"); // arquivo de conexão com o banco
 
-const usuario = require("./src/router/usuario.router");
+const usuario = require("./src/router/usuario.router"); //arquivo de rota do usuário
 
 const app = express();
 
@@ -11,10 +11,10 @@ app.use(express.json());
 
 
 
-connectToDatabase();
+connectToDatabase(); //conectando com o banco
 
 
-app.use("/usuario", usuario);
+app.use("/usuario", usuario); // chamando a rota do usuário
 
 app.get("/", (req, res) => {
     res.send({
--- src/router/usuario.router.js
+++ src/router/usuario.router.js
@@ -2,17 +2,21 @@ const express = require("express");
 const router = express.Router();
 const usuarioController = require("../controller/usuario.controller");
 
+//ROTAS GET
 router.get("/findById/:id", usuarioController.findUserByIdController);
 router.get("/findAll", usuarioController.findAllUsersController);
 
+//ROTAS POST
 router.post("/create", usuarioController.createUserController);
 router.post("/addAdress/:id", usuarioController.addUserAddressController);
 router.post("/addFavProduct/:id", usuarioController.addUserFavProduct);
 
+//ROTAS PUT
 router.put("/update/:id", usuarioController.updateUserController);
 
+//ROTAS DELETE
 router.delete("/remove/:id", usuarioController.removeUserController);
 router.delete("/removeAddress", usuarioController.removeUserAddressController);
-router.delete("/removeFavProduct", usuarioController.removeUserFavProduct);
+router.delete("/removeFavProduct", usuarioController.removeUserFavProductController);
 
 module.exports = router;
