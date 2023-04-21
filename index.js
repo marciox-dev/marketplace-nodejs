04ff0e1c5bb37e001bd39a8c68e7356db1e970c2
Marcio <marcioxerfan@gmail.com>
2023-04-21 10:17:09 -0300

    broke: parei porque não consigo criar o token

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const connectToDatabase = require("./src/database/database"); // arquivo de conexão com o banco
 
 const usuario = require("./src/router/usuario.router"); //arquivo de rota do usuário
+const auth = require("./src/router/auth.router"); //arquivo dde rota  autorização
 
 const app = express();
 
@@ -15,6 +16,7 @@ connectToDatabase(); //conectando com o banco
 
 
 app.use("/usuario", usuario); // chamando a rota do usuário
+app.use("/auth", auth); // chamando a rota do login
 
 app.get("/", (req, res) => {
     res.send({
--- src/router/usuario.router.js
+++ src/router/usuario.router.js
@@ -2,8 +2,10 @@ const express = require("express");
 const router = express.Router();
 const usuarioController = require("../controller/usuario.controller");
 
+const authMiddleware = require("../middleware/auth.middleware");
+
 //ROTAS GET
-router.get("/findById/:id", usuarioController.findUserByIdController);
+router.get("/findById/:id", authMiddleware, usuarioController.findUserByIdController);
 router.get("/findAll", usuarioController.findAllUsersController);
 
 //ROTAS POST
