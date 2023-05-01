ae4537c64fa4f8fafbb6dcdc6f6aed7b7e04c3ee
Marcio <marcioxerfan@gmail.com>
2023-05-01 16:33:09 -0300

    Feat CRUD PEDIDO+THUNDER Collection e env

--- src/controller/pedido.controller.js
+++ src/controller/pedido.controller.js
@@ -40,11 +40,23 @@ const deletePedidoController = async (req, res) => {
     }
 };
 
+const updateStatusPedidoController = async (req, res) => {
+    try{
+        res.status(200).send(await pedidoService.updateStatusPedidoService(req.params.id));
+    }catch(err){
+        console.log(`erro: ${err.message}`);
+        return res.status(500).send({ message: `Erro inesperado. Tente novamente!` });
+    }
+}
+
+
+
 
 
 module.exports = {
     findPedidoByIdController,
     findAllPedidosController,
     createPedidoController,
-    deletePedidoController
+    deletePedidoController,
+    updateStatusPedidoController
 }
