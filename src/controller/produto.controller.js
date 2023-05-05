c88b3987463e829f1e3217d53ac8137619cf79ce
Marcio <marcioxerfan@gmail.com>
2023-05-05 11:47:56 -0300

    Feat Fix

--- src/controller/produto.controller.js
+++ src/controller/produto.controller.js
@@ -1,50 +1,50 @@
 const produtoService = require("../service/produto.service");
 
 const findProductByIdController = async (req, res) => {
-    try{
+    try {
         res.send(await produtoService.findProductByIdService(req.params.id));
-    }catch(err){
+    } catch (err) {
         console.log(`err: ${err.message}`);
         return res.status(500).send({ message: `Erro inesperado. Tente novamente!` });
     }
 }
 
 const findAllProductsController = async (req, res) => {
-    try{
+    try {
         res.send(await produtoService.findAllProductsService(req.query.limit, req.query.offset));
-    }catch(err){
+    } catch (err) {
         console.log(`erro: ${err.message}`);
         return res.status(500).send({ message: `Erro inesperado. Tente novamente!` });
     }
 };
 
 const createProductController = async (req, res) => {
-    try{
+    try {
         const corpo = {
             ...req.body,
             userId: req.userId
         }
 
         res.status(201).send(await produtoService.createProductService(corpo));
-    }catch(err){
+    } catch (err) {
         console.log(`erro: ${err.message}`);
         return res.status(500).send({ message: `Erro inesperado. Tente novamente!` });
     }
 };
 
 const updateProductController = async (req, res) => {
-    try{
+    try {
         res.send(await produtoService.updateProductService(req.params.id, req.body));
-    }catch(err){
+    } catch (err) {
         console.log(`erro: ${err.message}`);
         return res.status(500).send({ message: `Erro inesperado. Tente novamente!` });
     }
 };
 
 const deleteProductController = async (req, res) => {
-    try{
+    try {
         res.send(await produtoService.deleteProductService(req.params.id));
-    }catch(err){
+    } catch (err) {
         console.log(`err: ${err.message}`);
         return res.status(500).send({ message: `Erro inesperado. Tente novamente!` });
     }
@@ -52,21 +52,18 @@ const deleteProductController = async (req, res) => {
 
 
 const addCategoriaProdutoController = async (req, res) => {
-    try{
-       
-            const categoria = await produtoService.addCategoriaProdutoService(req.params.id, req.body);
-            res.status(200).send(categoria);
-    }catch(err){
+    try {
+        res.status(200).send(await produtoService.addCategoriaProdutoService(req.params.id, req.body));
+    } catch (err) {
         console.log(`erro: ${err.message}`);
-        return res.status(500).send({ message: `Erro inesperado, tente novamente!`});
+        return res.status(500).send({ message: `Erro inesperado, tente novamente!` });
     }
 }
 
-const removeCategoriaProdutoController = async (req,res) =>{
-    try{
-        const categoria = await produtoService.removeCategoriaProdutoService(req.params.id, req.body);
-        res.status(200).send(categoria);
-    }catch(err){
+const removeCategoriaProdutoController = async (req, res) => {
+    try {
+        res.status(200).send(await produtoService.removeCategoriaProdutoService(req.params.id, req.body));
+    } catch (err) {
         console.log(`err: ${err.message}`);
         return res.status(500).send({ message: `Erro inesperado. Tente novamente!` });
     }
--- src/router/usuario.router.js
+++ src/router/usuario.router.js
@@ -4,24 +4,24 @@ const usuarioController = require("../controller/usuario.controller");
 const paginacao = require("../middleware/paginacao.middleware")
 
 const authMiddleware = require("../middleware/auth.middleware");
-const { validaUsuario, validaId } = require("../middleware/validacao.middleware")
+const { validaUsuario, validaEndereco, validaIdParams, valida_IdBody } = require("../middleware/validacao.middleware")
 
 
 //ROTAS GET
-router.get("/findById/:id", authMiddleware, validaId, usuarioController.findUserByIdController);
+router.get("/findById/:id", authMiddleware, validaIdParams, usuarioController.findUserByIdController);
 router.get("/findAll", authMiddleware, paginacao, usuarioController.findAllUsersController);
 
 //ROTAS POST
 router.post("/create", authMiddleware, validaUsuario, usuarioController.createUserController);
-router.post("/addAddress/:id", authMiddleware, validaId, usuarioController.addUserAddressController);
-router.post("/addFavProduct/:id", authMiddleware, validaId, usuarioController.addUserFavProductController);
+router.post("/addAddress/:id", authMiddleware, validaIdParams, validaEndereco, usuarioController.addUserAddressController);
+router.post("/addFavProduct/:id", authMiddleware, validaIdParams, valida_IdBody, usuarioController.addUserFavProductController);
 
 //ROTAS PUT
-router.put("/update/:id", authMiddleware, validaId, validaUsuario, usuarioController.updateUserController);
+router.put("/update/:id", authMiddleware, validaIdParams, validaUsuario, usuarioController.updateUserController);
 
 //ROTAS DELETE
-router.delete("/remove/:id", authMiddleware, validaId, usuarioController.removeUserController);
+router.delete("/remove/:id", authMiddleware, validaIdParams, usuarioController.removeUserController);
 router.delete("/removeAddress", authMiddleware, usuarioController.removeUserAddressController);
-router.delete("/removeFavProduct/:id", authMiddleware, validaId, usuarioController.removeUserFavProductController);
+router.delete("/removeFavProduct/:id", authMiddleware, validaIdParams, usuarioController.removeUserFavProductController);
 
 module.exports = router;
